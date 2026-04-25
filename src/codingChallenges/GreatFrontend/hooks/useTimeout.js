fdcb98dca411c16a735a3a22d8f873aeb707414d
Prasan RKP <prasan543e@gmail.com>
2026-04-25 19:32:12 +0530

    done a custom hook 'useTimeout.js'

---
+++ src/codingChallenges/GreatFrontend/hooks/useTimeout.js
@@ -0,0 +1,12 @@
+import {useEffect} from 'react';
+export default function useTimeout(callback, delay) {
+
+   useEffect(()=> {
+    
+       const timer = setTimeout(()=> {
+        callback();
+       }, delay);
+
+       return () => clearTimeout(timer);
+   }, [callback, delay]);
+}
