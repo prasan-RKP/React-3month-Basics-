dbb363c8df2b9c530e38c834129ccc901f553164
Prasan RKP <prasan543e@gmail.com>
2026-04-27 17:41:17 +0530

    practiced triaffic question

--- src/codingChallenges/GreatFrontend/TrafficLight.jsx
+++ src/codingChallenges/GreatFrontend/TrafficLight.jsx
@@ -1,36 +1,37 @@
-import {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react'
 
-export default function TrafficLight() {
+const TrafficLight = () => {
 
-const [light, setLight] = useState("green");
+  const [light, setLight] = useState('green');
 
-useEffect(()=> {
+  useEffect(() => {
+    let timer;
+    if (light === "green") {
+      timer = setTimeout(() => setLight("yellow"), 4000);
+    }
 
-let timer;
+    if (light === "yellow") {
+      timer = setTimeout(() => setLight("red"), 3000);
+    }
 
-if(light === "green"){
-  timer = setTimeout(()=> setLight("yellow"), 3000)
+    if (light === "red") {
+      timer = setTimeout(() => setLight("green"), 1500);
+    }
+
+    return () => {
+      clearInterval(timer);
+    }
+  }, [light])
+
+  return (
+    <div className='min-h-screen bg-black flex justify-center items-center'>
+      <div className='bg-gray-900 h-[180px] w-[70px] rounded-xl flex items-center justify-center flex-col gap-4'>
+        <div className={`${light === "red" ? "bg-red-500": "bg-gray-400"} w-10 h-10 rounded-full `}></div>
+        <div className={`${light === "yellow" ? "bg-yellow-500": "bg-gray-400"} w-10 h-10 rounded-full `}></div>
+        <div className={`${light === "green" ? "bg-green-500": "bg-gray-400"} w-10 h-10 rounded-full `}></div>
+      </div>
+    </div>
+  )
 }
-else if(light === "yellow"){
-  timer = setTimeout(()=> setLight("red"), 500)
-}
-else if(light === "red"){
-  timer = setTimeout(()=> setLight("green"), 4000)
-}
 
-return () => {
-  clearInterval(timer);
-}
-
-}, [light]);
-
-  return(
-<>
-    <div className="parent">
-         <div className = "light" style={{backgroundColor:light === "red" ? "red": "gray"}}> </div>
-         <div className = "light" style={{backgroundColor:light === "yellow" ? "yellow": "gray"}}> </div>
-          <div className="light" style={{backgroundColor:light === "green" ? "green": "gray"}}> </div>
-     </div>
-</>
-  ) 
-}
+export default TrafficLight;
