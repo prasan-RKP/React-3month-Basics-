42a20ac5189e4dfba7a4f25640fc3852302eb397
Prasan RKP <prasan543e@gmail.com>
2026-04-28 17:27:12 +0530

    practiced digi clock question

--- src/codingChallenges/GreatFrontend/DigitalClock.jsx
+++ src/codingChallenges/GreatFrontend/DigitalClock.jsx
@@ -1,23 +1,35 @@
-import {useEffect, useState}  from 'react';
+import React, { useEffect, useState } from 'react'
 
-export default function DigitalClock() {
+const DigitalClock = () => {
 
-     const [time, setTime] = useState(new Date());
+  const [timer, setTimer] = useState(new Date());
 
 
-      useEffect(()=> {
-        const timer = setInterval(()=> {
-          setTime(new Date());
-        }, 1000);
+  useEffect(()=> {
+       let timer;
+       setInterval(() => {
+        setTimer(new Date());
+       }, 1000);
 
-        return () => clearInterval(timer);
-      }, []);
+       return () => {
+        clearInterval(timer);
+       }
+  }, []);
 
-   
-    const hours = time.getHours();
-    const minutes = String(time.getMinutes()).padStart(2, "0");
-    const seconds = String(time.getSeconds()).padStart(2, "0");
-  return( 
-    <div className="clock">{hours}:{minutes}:{seconds}</div>
+
+  const hour = timer.getHours();
+  const minutes = String(timer.getMinutes()).padStart(2, "0");
+  const seconds = String(timer.getSeconds()).padStart(2, "0");
+
+
+
+  return (
+    <div className='min-h-screen flex justify-center items-center bg-black'>
+      <div className='flex justify-center items-center h-[50px] w-[150px] bg-red-600 p-5 rounded-lg'>
+        <p className='text-3xl text-white'>{hour}:{minutes}:{seconds}</p>
+      </div>
+    </div>
   )
 }
+
+export default DigitalClock;
